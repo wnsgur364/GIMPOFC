055f1f601a1f25356f604b44f40a885c36ccfcce
wnsgur364 <wnsgur364@naver.com>
2023-11-02 16:42:54 +0900

    DB 연동 성공

--- orange/src/main/java/com/podo/board/BoardApplication.java
+++ orange/src/main/java/com/podo/board/BoardApplication.java
@@ -1,4 +1,4 @@
-package com.example.board;
+package com.podo.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- orange/src/main/java/com/podo/board/controller/HomeContoller.java
+++ orange/src/main/java/com/podo/board/controller/HomeContoller.java
@@ -1,11 +1,11 @@
-package com.example.board.controller;
+package com.podo.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeContoller {
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
     }
