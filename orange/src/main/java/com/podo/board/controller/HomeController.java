db115a19b6bc6e962e17ba3b4a626c1702372028
wnsgur364 <wnsgur364@naver.com>
2023-11-03 01:32:50 +0900

    111

--- orange/src/main/java/com/podo/board/controller/HomeController.java
+++ orange/src/main/java/com/podo/board/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeContoller {
+public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
